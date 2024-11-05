90b095761b3c3bf7c6b18a11b55f95d31c12cf37
SyouTono242 <charlottesyr@outlook.com>
2024-11-05 17:07:53 -0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='OGGTA-Extra-Shift',
     version='1.0',
-    packages=['appnope==0.1.4', 'asttokens==2.4.1', 'attrs==24.2.0', 'autocommand==2.2.2', 'backports.tarfile==1.2.0', 'certifi==2024.8.30', 'charset-normalizer==3.4.0', 'comm==0.2.2', 'debugpy==1.8.7', 'decorator==5.1.1', 'executing==2.1.0', 'h11==0.14.0', 'idna==3.10', 'importlib-metadata==8.0.0', 'importlib-resources==6.4.0', 'inflect==7.3.1', 'ipykernel==6.29.5', 'ipython==8.29.0', 'jaraco.collections==5.1.0', 'jaraco.context==5.3.0', 'jaraco.functools==4.0.1', 'jaraco.text==3.12.1', 'jedi==0.19.1', 'jupyter-client==8.6.3', 'jupyter-core==5.7.2', 'matplotlib-inline==0.1.7', 'more-itertools==10.3.0', 'nest-asyncio==1.6.0', 'outcome==1.3.0.post0', 'packaging==24.1', 'parso==0.8.4', 'pexpect==4.9.0', 'pip==24.2', 'platformdirs==4.3.6', 'plyer==2.1.0', 'prompt-toolkit==3.0.48', 'psutil==6.1.0', 'ptyprocess==0.7.0', 'pure-eval==0.2.3', 'pygments==2.18.0', 'pyobjus==1.2.3', 'pysocks==1.7.1', 'python-dateutil==2.9.0.post0', 'pyzmq==26.2.0', 'requests==2.32.3', 'selenium==4.26.1', 'setuptools==75.1.0', 'six==1.16.0', 'sniffio==1.3.1', 'sortedcontainers==2.4.0', 'stack-data==0.6.3', 'tomli==2.0.1', 'tornado==6.4.1', 'tqdm==4.66.6', 'traitlets==5.14.3', 'trio-websocket==0.11.1', 'trio==0.27.0', 'typeguard==4.3.0', 'typing-extensions==4.12.2', 'urllib3==2.2.3', 'wcwidth==0.2.13', 'websocket-client==1.8.0', 'wheel==0.44.0', 'wsproto==1.2.0', 'zipp==3.19.2'],
+    packages=['OGGTA-Extra-Shift'],
     license='MIT License',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
 )
